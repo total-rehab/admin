3f69ad8d4b5b5264f0b5e0e88ba57eda3798177e
alexandermendes <alexander.mendes@immediate.co.uk>
2023-03-26 02:50:49 +0000

    Deploying to gh-pages from @ total-rehab/admin@cd356434764ad70f9adc4db77dd78cf4985c9c8f 🚀

---
+++ _next/static/C0FVIbkUuk7pz7djIWCTB/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-615c047843e82366.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
