3f5355a31f61908fc3a2bd44ef601d0d5c11b3d2
Alex Mendes <alexanderhmendes@gmail.com>
2023-03-26 05:32:35 +0100

    fix: plan and blog post includes

--- src/components/blog-posts/BlogPostEdit.tsx
+++ src/components/blog-posts/BlogPostEdit.tsx
@@ -3,10 +3,7 @@ import { FC } from 'react';
 import { BlogPostInputs } from './BlogPostInputs';
 
 export const BlogPostEdit: FC = () => (
-  <EditForm
-    queryOptions={{
-      meta: { include: { phases: true, programs: true } },
-    }}>
+  <EditForm>
     <BlogPostInputs />
   </EditForm>
 );
--- src/components/plans/PlanEdit.tsx
+++ src/components/plans/PlanEdit.tsx
@@ -3,10 +3,7 @@ import { FC } from 'react';
 import { PlanInputs } from './PlanInputs';
 
 export const PlanEdit: FC = () => (
-  <EditForm
-    queryOptions={{
-      meta: { include: { phases: true, programs: true } },
-    }}>
+  <EditForm>
     <PlanInputs />
   </EditForm>
 );
