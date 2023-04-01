1a826d5244a80904ece1b736af554a90e027449c
Alex Mendes <alexanderhmendes@gmail.com>
2023-04-01 16:32:38 +0100

    fix: sort phases in edit menu

--- src/components/programs/ProgramEdit.tsx
+++ src/components/programs/ProgramEdit.tsx
@@ -1,9 +1,20 @@
+import { ApiComponents } from '@jambff/oac';
 import { EditForm } from '@jambff/ra-components';
 import { FC } from 'react';
 import { ProgramInputs } from './ProgramInputs';
 
+const transform = (data: { phases: ApiComponents['ProgramPhase'][] }) => {
+  Object.assign(data, {
+    phases: data.phases.map((phase, i: number) => ({ ...phase, rank: i })),
+  });
+
+  return data;
+};
+
 export const ProgramEdit: FC = () => (
-  <EditForm queryOptions={{ meta: { include: { phases: true } } }}>
+  <EditForm
+    queryOptions={{ meta: { include: { phases: ['phase'] } } }}
+    transform={transform}>
     <ProgramInputs />
   </EditForm>
 );
