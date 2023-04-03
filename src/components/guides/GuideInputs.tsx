aa6b1d0aec3b468090fe6ea51272089504520be5
Alex Mendes <alexanderhmendes@gmail.com>
2023-04-03 22:10:24 +0100

    fix: use a different input for guide programs and phases

--- src/components/guides/GuideInputs.tsx
+++ src/components/guides/GuideInputs.tsx
@@ -1,44 +1,60 @@
+import { StepFormIterator } from '@jambff/ra-components';
 import { sentenceCase } from 'change-case';
 import { MediaLibraryInput } from 'ra-supabase-media-library';
 import { FC } from 'react';
+import { useTheme } from '@mui/material';
 import {
+  ArrayInput,
+  AutocompleteInput,
   RaRecord,
-  ReferenceArrayInput,
+  ReferenceInput,
   required,
   TextInput,
 } from 'react-admin';
-import { AutocompleteArrayOfObjectsInput } from '../generic/AutocompleteArrayOfObjectsInput';
-
 import { FlexRow } from '../generic/FlexRow';
 import { EditorContent } from '../inputs/EditorContent';
 
-export const GuideInputs: FC = () => (
-  <>
-    <TextInput source="title" validate={[required()]} fullWidth />
-    <FlexRow>
-      <MediaLibraryInput source="heroImageId" label="Hero Image" />
-      <MediaLibraryInput source="thumbnailImageId" label="Thumbnail Image" />
-    </FlexRow>
-    <FlexRow>
-      <ReferenceArrayInput source="programs" reference="programs">
-        <AutocompleteArrayOfObjectsInput
-          fullWidth
-          label="Programs"
-          optionText={(record: RaRecord) =>
-            `${record.title} - ${
-              record.isForOver60s ? 'Over 60s' : 'Under 60s'
-            } - ${sentenceCase(record.activityLevel)}`
-          }
-        />
-      </ReferenceArrayInput>
-      <ReferenceArrayInput source="phases" reference="phases">
-        <AutocompleteArrayOfObjectsInput
-          fullWidth
-          label="Phases"
-          optionText="name"
-        />
-      </ReferenceArrayInput>
-    </FlexRow>
-    <EditorContent source="content" fullWidth />
-  </>
-);
+export const GuideInputs: FC = () => {
+  const { spacing } = useTheme();
+
+  return (
+    <>
+      <TextInput source="title" validate={[required()]} fullWidth />
+      <FlexRow>
+        <MediaLibraryInput source="heroImageId" label="Hero Image" />
+        <MediaLibraryInput source="thumbnailImageId" label="Thumbnail Image" />
+      </FlexRow>
+      <EditorContent source="content" fullWidth />
+      <ArrayInput source="phases" sx={{ marginBottom: spacing(4) }}>
+        <StepFormIterator addButtonText="Add phase">
+          <ReferenceInput source="id" reference="phases">
+            <AutocompleteInput
+              fullWidth
+              label="Phases"
+              source=""
+              optionText="name"
+            />
+          </ReferenceInput>
+        </StepFormIterator>
+      </ArrayInput>
+      <ArrayInput source="programs" sx={{ marginBottom: spacing(4) }}>
+        <StepFormIterator addButtonText="Add program">
+          <ReferenceInput source="id" reference="programs">
+            <AutocompleteInput
+              fullWidth
+              label="Programs"
+              source=""
+              optionText={(record: RaRecord) => {
+                console.log('opt', record);
+
+                return `${record.title} - ${
+                  record.isForOver60s ? 'Over 60s' : 'Under 60s'
+                } - ${sentenceCase(record.activityLevel)}`;
+              }}
+            />
+          </ReferenceInput>
+        </StepFormIterator>
+      </ArrayInput>
+    </>
+  );
+};
