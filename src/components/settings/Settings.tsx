249f24d80145ca1ac13a5f7cfbd0f7671bdb7f75
Alex Mendes <alexanderhmendes@gmail.com>
2023-04-09 13:20:51 +0100

    feat: add terms and conditions setting

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -1,4 +1,5 @@
 import { ApiComponents } from '@jambff/oac';
+import { useTheme } from '@mui/material';
 import { createAuthenticatedFetch } from '@jambff/supabase-auth-fetch';
 import { FC, useCallback, useEffect, useState } from 'react';
 import {
@@ -10,6 +11,7 @@ import {
 } from 'react-admin';
 import { FieldValues } from 'react-hook-form';
 import { createSupabaseClient } from '../../supabase';
+import { EditorContent } from '../inputs/EditorContent';
 
 const supabase = createSupabaseClient();
 const fetch = createAuthenticatedFetch(supabase);
@@ -18,6 +20,7 @@ const settingsEndpoint = new URL('/settings', process.env.API_BASE_URL).href;
 export const Settings: FC = () => {
   const [settings, setSettings] = useState<ApiComponents['Settings']>();
   const notify = useNotify();
+  const { spacing } = useTheme();
 
   const loadSettings = useCallback(async () => {
     const res = await fetch(settingsEndpoint);
@@ -53,25 +56,34 @@ export const Settings: FC = () => {
   }
 
   return (
-    <SimpleForm record={settings} onSubmit={updateSettings}>
-      <NumberInput
-        fullWidth
-        validate={required()}
-        source="freeDays"
-        helperText="The number of days a user can complete before subscribing"
-      />
-      <NumberInput
-        fullWidth
-        validate={required()}
-        source="reviewDay"
-        helperText="The day that a request to leave a review is shown"
-      />
-      <TextInput
-        fullWidth
-        validate={required()}
-        source="remindersMessage"
-        helperText="The body of the scheduled reminder message"
-      />
+    <SimpleForm record={settings} onSubmit={updateSettings} sx={{ gap: 100 }}>
+      <div
+        style={{
+          gap: spacing(4),
+          width: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+        }}>
+        <NumberInput
+          fullWidth
+          validate={required()}
+          source="freeDays"
+          helperText="The number of days a user can complete before subscribing"
+        />
+        <NumberInput
+          fullWidth
+          validate={required()}
+          source="reviewDay"
+          helperText="The day that a request to leave a review is shown"
+        />
+        <TextInput
+          fullWidth
+          validate={required()}
+          source="remindersMessage"
+          helperText="The body of the scheduled reminder message"
+        />
+        <EditorContent source="termsAndConditions" fullWidth />
+      </div>
     </SimpleForm>
   );
 };
