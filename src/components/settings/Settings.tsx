eb25c60d71f6bf5320c83d3c5a582507af0ad799
Alex Mendes <alexanderhmendes@gmail.com>
2023-04-30 10:18:59 +0100

    feat: add citations

--- src/components/settings/Settings.tsx
+++ src/components/settings/Settings.tsx
@@ -85,6 +85,7 @@ export const Settings: FC = () => {
         />
         <EditorContent source="termsAndConditions" fullWidth />
         <EditorContent source="privacyPolicy" fullWidth />
+        <EditorContent source="citations" fullWidth />
       </div>
     </SimpleForm>
   );
