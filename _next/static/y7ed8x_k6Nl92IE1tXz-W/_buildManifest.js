a57713644b0238a1b8f3a50004df602324ed719b
m-junaid-butt <junaidbutt.dev@gmail.com>
2023-11-14 12:43:14 +0000

    Deploying to gh-pages from @ total-rehab/admin@11ae7693bb8331fd59805831b0bf16301352769f 🚀

---
+++ _next/static/y7ed8x_k6Nl92IE1tXz-W/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cabfead842c5994b.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
