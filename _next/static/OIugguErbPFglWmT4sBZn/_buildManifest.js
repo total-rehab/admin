1df7eb2a96cb4b6b86bc11a9056266d0562bb92b
alexandermendes <alexandermendes@users.noreply.github.com>
2024-01-16 00:35:11 +0000

    Deploying to gh-pages from @ total-rehab/admin@b96e1f4e52555fe9c1e691ef3088dc344bd978bf 🚀

--- _next/static/OIugguErbPFglWmT4sBZn/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1794739b54501c4d.js"],"/_error":["static/chunks/pages/_error-409f831d3504c8f5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
